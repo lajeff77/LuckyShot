35d5ac7801491a9a23b988a914cdf9d3312dd4b5
lajeff77 <lajeff77@gmail.com>
2019-01-10 22:44:06 -0800

    Initial Commit
    -created launcher

---
+++ src/main/Launcher.java
@@ -0,0 +1,20 @@
+/**
+ * <h2>Launcher class</h2>
+ *
+ * <p>This class is the main launching point of
+ * the game.</p>
+ *
+ * <p>Created:1/10/18</p>
+ *
+ * @author Lauryn Jefferson
+ * @version 1/10/18
+ */
+package main;
+
+public class Launcher
+{
+    public static void main(String[] args)
+    {
+        //Launch the game
+    }
+}
